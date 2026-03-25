3524659ee6322f4549a1655565b248753ffd1020
Dmitry SPF <dizscope@gmail.com>
2026-03-25 14:23:45 +0300

    dotenv

--- ex03_js/index.js
+++ ex03_js/index.js
@@ -1,7 +1,15 @@
 import Square, {size1, size2} from "./lib.js";
+import { config } from "dotenv";
+
+config();
+
+import { env } from "node:process";
 
 const mySquare = new Square(size1);
 console.log(`The area of mySquare is ${mySquare.area()}`);
 
 const mySquare2 = new Square(size2);
 console.log(`The area of mySquare is ${mySquare2.area()}`);
+
+console.log("Env test:", env.qwe);
+
